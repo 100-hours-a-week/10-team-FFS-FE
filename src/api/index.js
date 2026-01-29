4261551f8e8ab41db024053447099dcfc5efa2f8
dragonwaterr <yongsu626@naver.com>
2026-01-29 15:31:13 +0900

    chore: baseURL 환경 변수명 REACT_APP_ 접두어를 붙여서 undefined 방지

--- src/api/index.js
+++ src/api/index.js
@@ -3,7 +3,7 @@
    모든 API 함수를 여기서 관리합니다.
    ============================================== */
 
-const BASE_URL = process.env.BASE_URL;
+const BASE_URL = process.env.REACT_APP_BASE_URL || 'http://localhost:8080/api/v1';
 
 // 토큰 관리
 const getAccessToken = () => localStorage.getItem('accessToken');
